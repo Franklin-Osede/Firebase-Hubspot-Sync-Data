ad6fc47d66f167d67b8effe96aff4c49757d600f
Franklin <71532004+fran1014@users.noreply.github.com>
2025-02-10 10:15:56 +0100

    updated respomnses status in case of error

--- src/functions/controllers/syncController.js
+++ src/functions/controllers/syncController.js
@@ -1,10 +1,23 @@
-const syncUsersService = require('../services/syncUsersService');
+const functions = require('firebase-functions');
+const syncUsersService = require('./services/syncUsersService');
 
-exports.syncUsers = async (context) => {
+exports.syncUsers = functions.https.onRequest(async (req, res) => {
   try {
     const result = await syncUsersService.syncUsersFromExcel();
     console.log(result.message);
+
+    res.status(200).json({
+      success: result.success,
+      message: result.message,
+      updatedCount: result.updatedCount,
+      errorCount: result.errorCount,
+    });
   } catch (error) {
     console.error('Error en la sincronización de usuarios:', error);
+    res.status(500).json({
+      success: false,
+      message: 'Error en la sincronización de usuarios',
+      error: error.message,
+    });
   }
-};
+});
